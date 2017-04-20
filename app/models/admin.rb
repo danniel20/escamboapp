f83647db13b275ab4f4aea9fcfdb3b6bc1f83517
Danniel <danniel.master@gmail.com>
2017-04-20 12:15:42 -0300

    adicionando enum para role de administradores

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,17 @@
 class Admin < ActiveRecord::Base
+  enum role: [:full_access, :resticted_access]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def role_br
+    if self.role == 'full_access'
+      "Acesso Completo"
+    else
+      "Acesso Restrito"
+    end
+
+  end
+
 end
