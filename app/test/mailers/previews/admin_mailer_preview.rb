ff7d8e6ff623b4ca966cdad9b8985eb6819d28f0
Danniel Albuquerque <danniel.master@gmail.com>
2017-05-09 09:26:58 -0300

    ajuste preview de envio de email

--- app/test/mailers/previews/admin_mailer_preview.rb
+++ app/test/mailers/previews/admin_mailer_preview.rb
@@ -4,4 +4,8 @@ class AdminMailerPreview < ActionMailer::Preview
   	AdminMailer.update_email(Admin.first, Admin.last)
   end
 
+  def method_name
+  	AdminMailer.send_message(Admin.first.email, Admin.last.email, "Subject Test", "Lorem Lorem")
+  end
+
 end
