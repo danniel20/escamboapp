284b2ee6ee0b120ef194109ad0b27061a5567eba
Danniel Albuquerque <danniel.master@gmail.com>
2017-05-12 11:53:12 -0300

    ajustes e diagram ER

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,8 @@
 class Ad < ActiveRecord::Base
+
+  # Constants
+  QTT_PER_PAGE = 6
+
   # Callbacks
   before_save :md_to_html
 
@@ -10,10 +14,10 @@ class Ad < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0 }
 
   # Scopes
-  scope :descending_order, -> (quantity = 10, page = 1) { limit(quantity).order(created_at: :desc).page(page).per(6)}
+  scope :descending_order, -> (page) { order(created_at: :desc).page(page).per(QTT_PER_PAGE)}
   scope :to_the, -> (member) { where(member: member)}
   scope :by_category, -> (id) { where(category: id)}
-  scope :search, -> (term, page = 1) { where("lower(title) LIKE ?", "%#{term.downcase}%").page(page).per(6)}
+  scope :search, -> (term, page) { where("lower(title) LIKE ?", "%#{term.downcase}%").page(page).per(QTT_PER_PAGE)}
 
   # paperclip
   has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
