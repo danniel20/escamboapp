9224736bbbe2385430ad8524395c9cdf37e4c1cf
Danniel Albuquerque <danniel.master@gmail.com>
2017-05-15 12:03:49 -0300

    nested attributes com devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512183741) do
+ActiveRecord::Schema.define(version: 20170515133905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,17 @@ ActiveRecord::Schema.define(version: 20170512183741) do
     t.datetime "updated_at"
   end
 
+  create_table "profile_members", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "second_name"
+    t.date     "birthdate"
+    t.integer  "member_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "profile_members", ["member_id"], name: "index_profile_members_on_member_id", using: :btree
+
   create_table "rates", force: :cascade do |t|
     t.integer  "rater_id"
     t.integer  "rateable_id"
@@ -154,4 +165,5 @@ ActiveRecord::Schema.define(version: 20170512183741) do
   add_foreign_key "ads", "members"
   add_foreign_key "comments", "ads"
   add_foreign_key "comments", "members"
+  add_foreign_key "profile_members", "members"
 end
