7629c16077c7434af66b0373e77736151498627b
Danniel Albuquerque <danniel.master@gmail.com>
2017-05-24 10:24:55 -0300

    processando o pagamento

--- app/controllers/checkout/payments_controller.rb
+++ app/controllers/checkout/payments_controller.rb
@@ -7,7 +7,26 @@ class Checkout::PaymentsController < ApplicationController
 		order = Order.create(ad: ad, buyer_id: current_member.id)
 		order.waiting!
 
-		render(text: "Processando.... Pedido #{order.status_i18n} - Anúncio #{ad.status_i18n}")
+		payment = PagSeguro::PaymentRequest.new
+
+		payment.reference = order.id
+		payment.notification_url = root_url # FIX LATER
+		payment.redirect_url = site_ad_detail_url(ad)
+
+		payment.items << {
+			id: ad.id,
+			description: ad.title,
+			amount: ad.price.to_s.gsub(',', '.')
+		}
+
+		response = payment.register
+
+		if response.errors.any?
+			redirect_to(site_ad_detail_path(ad), alert: "Erro ao processar comrpa.. Entre em contato com o SAC")
+		else
+			redirect_to(response.url)
+		end
+
 	end
 
 end
