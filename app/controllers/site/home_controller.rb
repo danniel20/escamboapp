f86d481a78a6adb2112133e59b13a004a704e544
Danniel Albuquerque <danniel.master@gmail.com>
2017-05-30 11:54:50 -0300

    utilizando sidekiq para enfileirar jobs

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,6 +1,7 @@
 class Site::HomeController < SiteController
 
   def index
+  	HardWorker.perform_async("Teste de envio de mensagem")
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.random(3)
